be2eca7831d6cf92f05dfef36c198a9d751203ac
Ben Berger <ben@Bens-MacBook-Pro.local>
2023-01-17 22:42:04 -0500

    Changed ggplot font.

--- R/theme_bergr.R
+++ R/theme_bergr.R
@@ -3,7 +3,7 @@
 #' @export
 theme_bergr <- function(){
   ggplot2::theme_void() +
-    hrbrthemes::theme_ipsum_rc() +
+    hrbrthemes::theme_ipsum() +
     ggplot2::theme(plot.title        = ggplot2::element_text(hjust = 0.5),
                    plot.subtitle     = ggplot2::element_text(hjust = 0.5),
                    axis.title.y      = ggplot2::element_text(hjust = 0.5, size = 12),
